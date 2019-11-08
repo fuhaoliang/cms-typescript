1e17169423a775245007bfecfcb1bd0decc56201
付豪亮 <fuhl@fosun.com>
2019-11-08 11:04:06 +0800

    上传优化

--- src/controllers/common.ts
+++ src/controllers/common.ts
@@ -10,14 +10,13 @@ export class CommonControllers {
       const file = ctx.request.files.file; // 获取上传文件
       // 创建可读流
       const reader = fs.createReadStream(file.path);
-      const type = file.type.split('/')[1]
-      let filePath: string = path.join(__dirname, '../../upload') + `/${file.name}.${type}`;
+      let filePath: string = path.join(__dirname, '../../upload') + `/${file.name}`;
       // 创建可写流
       const upStream = fs.createWriteStream(filePath);
       // 可读流通过管道写入可写流
       reader.pipe(upStream);
       ctx.response.status = 200;
-      ctx.body = statusCode.SUCCESS_200('ok', { file: `${appUrl.uploadUrl}/${file.name}.${type}` });
+      ctx.body = statusCode.SUCCESS_200('ok', { file: `${appUrl.uploadUrl}/${file.name}` });
     } catch (err) {
       ctx.response.status = 412;
       ctx.body = statusCode.ERROR_412('上传失败');
--- src/middlrware/update.ts
+++ src/middlrware/update.ts
@@ -3,6 +3,7 @@ const koaBody = require('koa-body');
 export const updateMiddlrware = koaBody({
   multipart: true,
   formidable: {
+    keepExtensions: true, // 保持文件的后缀
     maxFileSize: 200 * 1024 * 1024    // 设置上传文件大小最大限制，默认2M
   }
 });
